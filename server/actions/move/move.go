fd6e1d8e7a56284493717bdf67b63ea24550460d
Ian Armour <ianmarmour@gmail.com>
2020-05-04 12:59:20 -0700

    Removed extra log statement

--- server/actions/move/move.go
+++ server/actions/move/move.go
@@ -1,7 +1,6 @@
 package move
 
 import (
-	"log"
 	"math"
 
 	deviant "github.com/recluse-games/deviant-protobuf/genproto/go"
@@ -9,8 +8,6 @@ import (
 
 // Move draws a card for entity.
 func Move(encounter *deviant.Encounter, moveAction *deviant.EntityMoveAction) bool {
-	log.Output(1, "test move update")
-
 	// Get the absolute value of the difference between all of our positions on the board.
 	totalApCost := math.Abs(float64(moveAction.FinalXPosition - moveAction.StartXPosition + moveAction.FinalYPosition - moveAction.StartYPosition))
 
