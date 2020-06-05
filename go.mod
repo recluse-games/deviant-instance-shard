1239fede59cb80bf89fad3a53102db30ea63880b
Ian Armour <ianmarmour@gmail.com>
2020-06-04 21:26:22 -0700

    Added a rotation action

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/uuid v1.1.1
-	github.com/recluse-games/deviant-protobuf v0.0.0-20200604053734-ad6a960ef0b8
+	github.com/recluse-games/deviant-protobuf v0.0.0-20200605041419-81241742a198
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
 	google.golang.org/genproto v0.0.0-20200604104852-0b0486081ffb // indirect
--- server/actions/rotate/rotate.go
+++ server/actions/rotate/rotate.go
@@ -5,13 +5,8 @@ import (
 )
 
 // Rotate Rotates an Entity Based on a Tile Selection
-func Rotate(encounter *deviant.Encounter, moveAction *deviant.EntityMoveAction) bool {
-	encounter.ActiveEntity.Ap = encounter.ActiveEntity.Ap - apCostX
-	encounter.ActiveEntity.Ap = encounter.ActiveEntity.Ap - apCostY
-
-	// Apply all state changes to entity in encounter as well as the activeEntity.
-	encounter.Board.Entities.Entities[moveAction.FinalXPosition].Entities[moveAction.FinalYPosition] = encounter.ActiveEntity
-	encounter.Board.Entities.Entities[moveAction.StartXPosition].Entities[moveAction.StartYPosition] = &deviant.Entity{}
+func Rotate(encounter *deviant.Encounter, entityRotateAction *deviant.EntityRotateAction) bool {
+	encounter.ActiveEntity.Rotation = entityRotateAction.Rotation
 
 	return true
 }
