98af8eb7b42503124f41dc546fab82bc78808ede
Ian Armour <ianmarmour@gmail.com>
2020-06-30 23:41:13 -0700

    Updating x and y

--- server/actions/deck/deck.go
+++ server/actions/deck/deck.go
@@ -53,9 +53,9 @@ func DrawCard(encounter *deviant.Encounter) bool {
 			for y, entityRow := range encounter.Board.Entities.Entities {
 				for x := range entityRow.Entities {
 					// HACK - This logic should be moved outside of this method and processed on every turn or something.
-					if encounter.Board.Entities.Entities[x].Entities[y].Hp == 0 {
-						encounter.ActiveEntityOrder = removeEntityFromOrder(encounter.Board.Entities.Entities[x].Entities[y].Id, encounter.ActiveEntityOrder)
-						encounter.Board.Entities.Entities[x].Entities[y] = &deviant.Entity{}
+					if encounter.Board.Entities.Entities[y].Entities[x].Hp == 0 {
+						encounter.ActiveEntityOrder = removeEntityFromOrder(encounter.Board.Entities.Entities[y].Entities[x].Id, encounter.ActiveEntityOrder)
+						encounter.Board.Entities.Entities[y].Entities[x] = &deviant.Entity{}
 					}
 				}
 			}
