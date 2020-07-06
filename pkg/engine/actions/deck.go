4acbfabb03a0312467c47511eba2029f0289e7cf
Ian Armour <ianmarmour@gmail.com>
2020-07-06 12:34:19 -0700

    Updated conditional logic for drawing cards decrementing HP to support nil cards

--- pkg/engine/actions/deck.go
+++ pkg/engine/actions/deck.go
@@ -17,7 +17,7 @@ func removeCard(slice []*deviant.Card) []*deviant.Card {
 // DrawCard Draws a card for the currently active entity of an encounter.
 func DrawCard(encounter *deviant.Encounter, logger *zap.Logger) bool {
 	// WARNING: I don't love that this is included in the DrawCard logic, it feels like it should be somewhere else.
-	if len(encounter.ActiveEntity.Deck.Cards) == 0 {
+	if encounter.ActiveEntity.Deck.Cards == nil || len(encounter.ActiveEntity.Deck.Cards) == 0 {
 		encounter.ActiveEntity.Hp = encounter.ActiveEntity.Hp - 1
 
 		return true
