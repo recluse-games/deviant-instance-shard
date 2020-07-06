c65adc0117207a28e54b411754510163fc5191fd
Ian Armour <ianmarmour@gmail.com>
2020-07-06 10:05:22 -0700

    Only attempt to remove entities if they're actually in the turn order still

--- pkg/engine/actions/play.go
+++ pkg/engine/actions/play.go
@@ -115,7 +115,12 @@ func Play(encounter *deviant.Encounter, playAction *deviant.EntityPlayAction, lo
 
 					// HACK - This logic should be moved outside of this method and processed on every turn or something.
 					if encounter.Board.Entities.Entities[x].Entities[y].Hp <= 0 {
-						encounter.ActiveEntityOrder = removeEntityFromOrder(encounter.Board.Entities.Entities[x].Entities[y].Id, encounter.ActiveEntityOrder)
+						for _, entityID := range encounter.ActiveEntityOrder {
+							if entityID == encounter.Board.Entities.Entities[x].Entities[y].Id {
+								encounter.ActiveEntityOrder = removeEntityFromOrder(encounter.Board.Entities.Entities[x].Entities[y].Id, encounter.ActiveEntityOrder)
+							}
+						}
+
 						encounter.Board.Entities.Entities[x].Entities[y] = &deviant.Entity{}
 					}
 				}
